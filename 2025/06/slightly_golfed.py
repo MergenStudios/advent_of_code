b5b32402a270ce25c7bfd566af9d1aea8d29e2a1
MergenStudios <71030751+MergenStudios@users.noreply.github.com>
2025-12-07 02:46:19 +0200

    improved the slightly golfed version to include both parts

--- 2025/06/slightly_golfed.py
+++ 2025/06/slightly_golfed.py
@@ -1,7 +1,15 @@
 import sys, math
 
 lines = open(sys.argv[1]).readlines()
-total, stack, operators = 0, [], {"+": sum, "*": math.prod}
+operators = {"+": sum, "*": math.prod}
+
+# part 1
+total_p1 = 0
+for line in zip(*map(lambda x: x.split(), lines)):
+    total_p1 += operators[line[-1]](map(int, line[:-1]))
+
+# part 2
+total_p2, stack, operators = 0, [], {"+": sum, "*": math.prod} # part 2
 for c in list(zip(*map(lambda x: list(reversed(x)), lines)))[1:]:
     joined = "".join(c[:-1])
     if not joined.strip(): continue
@@ -9,7 +17,7 @@ for c in list(zip(*map(lambda x: list(reversed(x)), lines)))[1:]:
     stack.append(int(joined))
 
     if c[-1] in operators:
-        total += operators[c[-1]](stack)
+        total_p2 += operators[c[-1]](stack)
         stack.clear()
 
-print(total)
+print(total_p1, total_p2)
